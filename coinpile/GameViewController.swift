d948f685e9cf34aaf2b2944d5ea4ccfeeddc7294
Eric Akaoka <eakaoka@wealthsimple.com>
2017-10-27 23:22:02 -0400

    Normal map and spinning coin.

--- coinpile/GameViewController.swift
+++ coinpile/GameViewController.swift
@@ -30,25 +30,33 @@ class GameViewController: UIViewController {
         let lightNode = SCNNode()
         lightNode.light = SCNLight()
         lightNode.light!.type = .omni
-        lightNode.position = SCNVector3(x: 5, y: 5, z: -5)
+        lightNode.light?.intensity = 1000.0
+        lightNode.position = SCNVector3(x: 5, y: 5, z: 5)
         scene.rootNode.addChildNode(lightNode)
         
         // create and add an ambient light to the scene
         let ambientLightNode = SCNNode()
         ambientLightNode.light = SCNLight()
         ambientLightNode.light!.type = .ambient
-        ambientLightNode.light!.color = UIColor.gray
+        ambientLightNode.light!.color = UIColor(hue: 0.0, saturation: 0.0, brightness: 0.5, alpha: 1.0)
         scene.rootNode.addChildNode(ambientLightNode)
         
-        // retrieve the ship node
-        let coin = SCNCylinder(radius: 2, height: 0.4)
+        // configure coin node
+        let coin = SCNCylinder(radius: 2, height: 0.2)
         coin.firstMaterial?.lightingModel = .physicallyBased
         coin.firstMaterial?.diffuse.contents = UIColor(red: 0.85, green: 0.82, blue: 0.58, alpha: 1.0)
-        coin.firstMaterial?.roughness.contents = NSNumber(value: 0.5)
-        coin.firstMaterial?.metalness.contents = NSNumber(value: 1.0)
+//        coin.firstMaterial?.roughness.contents = 0.9
+        coin.firstMaterial?.metalness.contents = 1.0
+        
+        let coinFaceImage = UIImage(named: "art.scnassets/coin_normal_map.png")
+        coin.firstMaterial?.normal.contents = coinFaceImage
         
         let coinNode = SCNNode(geometry: coin)
         scene.rootNode.addChildNode(coinNode)
+        
+        let spin = SCNAction.rotateBy(x: CGFloat(Float.pi), y: 0.0, z: 0.0, duration: 1.0)
+        let action = SCNAction.repeatForever(spin)
+        coinNode.runAction(action)
         
         // retrieve the SCNView
         let scnView = self.view as! SCNView
@@ -64,7 +72,7 @@ class GameViewController: UIViewController {
         
         // configure the view
         scnView.backgroundColor = UIColor(red: 0.97, green: 0.97, blue: 0.96, alpha: 1.0)
-        scnView.autoenablesDefaultLighting = true
+//        scnView.autoenablesDefaultLighting = true
         
         // add a tap gesture recognizer
 //        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
