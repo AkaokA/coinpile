5d8066b0897178798a527a3e57b6aab1dbf22b58
Eric Akaoka <eakaoka@wealthsimple.com>
2017-11-09 20:41:20 -0500

    OMG accelorama.

--- coinpile/GameViewController.swift
+++ coinpile/GameViewController.swift
@@ -9,12 +9,15 @@
 import UIKit
 import QuartzCore
 import SceneKit
+import CoreMotion
 
 class GameViewController: UIViewController {
     
     var coinsAreFlowing = false
     var timer = Timer()
     var globalTimer = Timer()
+    let motionManager = CMMotionManager()
+    let bgColor = UIColor(red: 0.97, green: 0.97, blue: 0.96, alpha: 1.0)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,8 +31,8 @@ class GameViewController: UIViewController {
         scene.rootNode.addChildNode(cameraNode)
         
         // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: 6, z: 12)
-        cameraNode.eulerAngles = SCNVector3Make(-Float.pi/8, 0, 0);
+        cameraNode.position = SCNVector3(x: 0, y: 0.25, z: 3)
+        cameraNode.eulerAngles = SCNVector3(x: -Float.pi/8, y: 0, z: 0)
         
         // create and add a light to the scene
         let lightNode = SCNNode()
@@ -46,20 +49,59 @@ class GameViewController: UIViewController {
         ambientLightNode.light!.color = UIColor(hue: 0.0, saturation: 0.0, brightness: 1.0, alpha: 1.0)
         scene.rootNode.addChildNode(ambientLightNode)
         
+        // configure floor/ceiling node
+        let floor = SCNBox(width: 100, height: 1, length: 100, chamferRadius: 0.0)
+        floor.firstMaterial?.lightingModel = .constant
+        floor.firstMaterial?.diffuse.contents = bgColor
+        let floorNode = SCNNode(geometry: floor)
+        floorNode.position = SCNVector3(x: 0.0, y: -2.0, z: 0.0)
+        let floorPhysicsShape = SCNPhysicsShape(geometry: floor, options: nil)
+        floorNode.physicsBody = SCNPhysicsBody(type: .static, shape: floorPhysicsShape)
+        scene.rootNode.addChildNode(floorNode)
+        
+        let ceilingNode = SCNNode(geometry: floor)
+        ceilingNode.position = SCNVector3(x: 0.0, y: 2.5, z: 0.0)
+        ceilingNode.physicsBody = SCNPhysicsBody(type: .static, shape: floorPhysicsShape)
+        scene.rootNode.addChildNode(ceilingNode)
+        
+        // walls
+        let wallWidth = CGFloat(100)
+        let wallHeight = CGFloat(100)
+        let wall = SCNBox(width: wallWidth, height: wallHeight, length: 1, chamferRadius: 0)
+        wall.firstMaterial?.lightingModel = .constant
+        wall.firstMaterial?.diffuse.contents = bgColor
+        let wallPhysicsShape = SCNPhysicsShape(geometry: wall, options: nil)
+        
+        let backWallNode = SCNNode(geometry: wall)
+        backWallNode.position = SCNVector3(x: 0.0, y: 0.0, z: -1.5)
+        backWallNode.physicsBody = SCNPhysicsBody(type: .static, shape: wallPhysicsShape)
+        scene.rootNode.addChildNode(backWallNode)
+        
+        let frontWallNode = SCNNode(geometry: wall)
+        frontWallNode.position = SCNVector3(x: 0.0, y: 0.0, z: 2.0)
+        frontWallNode.physicsBody = SCNPhysicsBody(type: .static, shape: wallPhysicsShape)
+        scene.rootNode.addChildNode(frontWallNode)
+        
+        let leftWallNode = SCNNode(geometry: wall)
+        leftWallNode.position = SCNVector3(x: -1.75, y: 0.0, z: 0.0)
+        leftWallNode.eulerAngles = SCNVector3(x: 0, y: -Float.pi * 0.42, z: 0)
+        leftWallNode.physicsBody = SCNPhysicsBody(type: .static, shape: wallPhysicsShape)
+        scene.rootNode.addChildNode(leftWallNode)
+        
+        let rightWallNode = SCNNode(geometry: wall)
+        rightWallNode.position = SCNVector3(x: 1.75, y: 0.0, z: 0.0)
+        rightWallNode.eulerAngles = SCNVector3(x: 0, y: Float.pi * 0.42, z: 0)
+        rightWallNode.physicsBody = SCNPhysicsBody(type: .static, shape: wallPhysicsShape)
+        scene.rootNode.addChildNode(rightWallNode)
+        
         // configure physics simulation
         scene.physicsWorld.speed = 1.0
         
-        // configure floor node
-        let floor = SCNBox(width: 100, height: 5, length: 100, chamferRadius: 0.0)
-        floor.firstMaterial?.lightingModel = .constant
-        floor.firstMaterial?.diffuse.contents = UIColor(red: 0.97, green: 0.97, blue: 0.96, alpha: 1.0)
-        
-        let floorNode = SCNNode(geometry: floor)
-        floorNode.position = SCNVector3(x: 0.0, y: -4.0, z: 0.0)
-        let floorPhysicsShape = SCNPhysicsShape(geometry: floor, options: nil)
-        floorNode.physicsBody = SCNPhysicsBody(type: .static, shape: floorPhysicsShape)
-        
-        scene.rootNode.addChildNode(floorNode)
+        // global forces
+        let globalForceNode = SCNNode()
+        globalForceNode.name = "globalForceNode"
+        globalForceNode.physicsField = SCNPhysicsField.linearGravity()
+        scene.rootNode.addChildNode(globalForceNode)
         
         // retrieve the SCNView
         let scnView = self.view as! SCNView
@@ -74,7 +116,10 @@ class GameViewController: UIViewController {
 //        scnView.showsStatistics = true
         
         // configure the view
-        scnView.backgroundColor = UIColor(red: 0.97, green: 0.97, blue: 0.96, alpha: 1.0)
+        scnView.backgroundColor = bgColor
+        
+        
+        
         
         // add a tap gesture recognizer
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
@@ -87,6 +132,30 @@ class GameViewController: UIViewController {
         
         globalTimer = Timer.scheduledTimer(withTimeInterval: 30, repeats: false) { _ in
             self.timer.invalidate()
+        }
+        
+        
+
+        motionManager.startDeviceMotionUpdates()
+        _ = Timer.scheduledTimer(withTimeInterval: 0.01667, repeats: true) { _ in
+            self.motionUpdate()
+        }
+
+    }
+    
+    var accelVector = SCNVector3()
+    @objc func motionUpdate() {
+        
+        if let MotionData = self.motionManager.deviceMotion?.userAcceleration {
+            accelVector = SCNVector3(x: Float(MotionData.x), y: Float(MotionData.y), z: Float(MotionData.z))
+            let accelStrength = sqrtf( powf(accelVector.x, 2) + powf(accelVector.y, 2) + powf(accelVector.z, 2) )
+            
+            let scnView = self.view as! SCNView
+            
+            let globalForceNode = scnView.scene?.rootNode.childNode(withName: "globalForceNode", recursively: false)
+            globalForceNode?.physicsField?.direction = accelVector
+            globalForceNode?.physicsField?.strength = CGFloat(accelStrength * 9.8)
+            
         }
     }
     
@@ -106,7 +175,7 @@ class GameViewController: UIViewController {
     }
     
     func newCoin() -> SCNNode {
-        let coin = SCNCylinder(radius: 0.8, height: 0.16)
+        let coin = SCNCylinder(radius: 0.2, height: 0.04)
         coin.radialSegmentCount = 32
         
         coin.firstMaterial?.lightingModel = .physicallyBased
@@ -116,25 +185,23 @@ class GameViewController: UIViewController {
         coin.firstMaterial?.metalness.contents = NSNumber(value: 1.0)
         
         let coinNode = SCNNode(geometry: coin)
-        coinNode.position = SCNVector3(x: 0.0, y: 15.0, z: 0.0)
+        coinNode.position = SCNVector3(x: 0.0, y: 2.0, z: 0.0)
         
         let coinPhysicsShape = SCNPhysicsShape(geometry: coin, options: nil)
         coinNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: coinPhysicsShape)
-        coinNode.physicsBody?.mass = 0.2
-        coinNode.physicsBody?.friction = 1
+        coinNode.physicsBody?.mass = 1.0
+        coinNode.physicsBody?.friction = 0.5
         coinNode.physicsBody?.rollingFriction = 0.1
         coinNode.physicsBody?.damping = 0.1
         coinNode.physicsBody?.angularDamping = 0.25
         
         let randomPerc = CGFloat(arc4random()) / CGFloat(UInt32.max)
-        let coinTorque = SCNVector4(x: Float(randomPerc - 0.5), y: Float(randomPerc - 0.5), z: Float(randomPerc - 0.5), w: 1)
-        let coinForce = SCNVector3(x: Float(randomPerc - 0.5), y: -3, z: Float(randomPerc - 0.5))
+        let coinTorque = SCNVector4(x: Float(randomPerc - randomPerc/2), y: Float(randomPerc - randomPerc/2), z: Float(randomPerc - randomPerc/2), w: 0.5)
         coinNode.physicsBody?.applyTorque(coinTorque, asImpulse: true)
-        coinNode.physicsBody?.applyForce(coinForce, asImpulse: true)
         
-        Timer.scheduledTimer(withTimeInterval: 20, repeats: true) { _ in
-            coinNode.physicsBody?.type = .static
-        }
+//        Timer.scheduledTimer(withTimeInterval: 10, repeats: true) { _ in
+//            coinNode.physicsBody?.type = .static
+//        }
         
         return coinNode
     }
@@ -159,5 +226,7 @@ class GameViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-
+    
 }
+
+
