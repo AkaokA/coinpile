fd8bae06dd2428f6613eb37805b86ec009093711
Eric Akaoka <eakaoka@wealthsimple.com>
2017-11-29 17:32:56 -0500

    Really confused as to why the box goes where it's supposed to go but nothing else...?

--- coinpile/ARViewController.swift
+++ coinpile/ARViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     @IBOutlet var sceneView: ARSCNView!
     
+    let bgColor = UIColor(red: 0.97, green: 0.97, blue: 0.96, alpha: 1.0)
     var anchors = [ARAnchor]()
     
     override func viewDidLoad() {
@@ -28,6 +29,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Create a new scene
         let scene = SCNScene()
         sceneView.scene = scene
+        setUpLights()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -65,35 +67,43 @@ class ViewController: UIViewController, ARSCNViewDelegate {
      @return Node that will be mapped to the anchor or nil.
      */
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        var anchorNode:  SCNNode?
         
-        var node:  SCNNode?
         if let planeAnchor = anchor as? ARPlaneAnchor {
-            node = SCNNode()
-            // let planeGeometry = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
-            // let planeGeometry = SCNBox(width: CGFloat(planeAnchor.extent.x), height: planeHeight, length: CGFloat(planeAnchor.extent.z), chamferRadius: 0.0)
-//            let planeHeight:CGFloat = 0.01
+            anchorNode = SCNNode()
+            let anchorPosition = SCNVector3(planeAnchor.center.x, planeAnchor.center.y, planeAnchor.center.z)
             
-            let planeGeometry = SCNFloor()
-            planeGeometry.firstMaterial?.diffuse.contents = UIColor.clear
+            let planeGeometry = SCNBox(width: CGFloat(planeAnchor.extent.x), height: 0.01, length: CGFloat(planeAnchor.extent.z), chamferRadius: 0.0)
+            
+//            let planeGeometry = SCNFloor()
+            planeGeometry.firstMaterial?.diffuse.contents = UIColor.green
             let planePhysicsShape = SCNPhysicsShape(geometry: planeGeometry, options: nil)
             
             let planeNode = SCNNode(geometry: planeGeometry)
             planeNode.physicsBody = SCNPhysicsBody(type: .static, shape: planePhysicsShape)
-            planeNode.position = SCNVector3Make(planeAnchor.center.x, planeAnchor.center.y, planeAnchor.center.z)
+//            planeNode.position = anchorPosition // ??????
             
-            //since SCNPlane is vertical, needs to be rotated -90 degrees on X axis to make a plane
-//            planeNode.transform = SCNMatrix4MakeRotation(Float(-CGFloat.pi/2), 1, 0, 0)
+            print(anchorPosition)
+            print(planeNode.position)
             
-            node?.addChildNode(planeNode)
-            node?.addChildNode(self.newCoin())
+            // create and add a directional light to the scene
+            let lightNode = SCNNode()
+            lightNode.light = SCNLight()
+            lightNode.light!.type = .omni
+            lightNode.light?.intensity = 120
+            lightNode.position = anchorPosition
             
-            anchors.append(planeAnchor)
+            anchorNode?.addChildNode(lightNode)
+            anchorNode?.addChildNode(self.newCoin(position: anchorPosition))
+            anchorNode?.addChildNode(planeNode)
+            
+//            anchors.append(planeAnchor)
             
         } else {
             // haven't encountered this scenario yet
             print("not plane anchor \(anchor)")
         }
-        return node
+        return anchorNode
     }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
@@ -120,39 +130,24 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         ambientLightNode.light!.type = .ambient
         ambientLightNode.light!.color = UIColor(hue: 0.0, saturation: 0.0, brightness: 1.0, alpha: 1.0)
         sceneView.scene?.rootNode.addChildNode(ambientLightNode)
-        
-        // create and add a directional light to the scene
-        let lightNode = SCNNode()
-        lightNode.position = SCNVector3(x: -0.5, y: 2, z: 0.75)
-        
-        lightNode.light = SCNLight()
-        lightNode.light!.type = .omni
-        lightNode.light?.intensity = 120
-        lightNode.light?.spotInnerAngle = 45.0
-        lightNode.light?.spotOuterAngle = 90.0
-        lightNode.light?.castsShadow = true
-        lightNode.light?.shadowSampleCount = 8
-        lightNode.light?.shadowRadius = 2.0
-        lightNode.light?.shadowBias = 1.5
-        sceneView.scene?.rootNode.addChildNode(lightNode)
     }
     
-    func dropCoins(parentNode:SCNNode) {
-        let numberOfCoins:Double = 100
-        let coinsPerSecond:Double = 8
-        let coinInterval:Double = 1.0 / coinsPerSecond
-        let coinFlowDuration:Double = numberOfCoins/coinsPerSecond
-        
-        let coinFlowTimer = Timer.scheduledTimer(withTimeInterval: coinInterval, repeats: true) { _ in
-            parentNode.addChildNode(self.newCoin())
-        }
-        
-        _ = Timer.scheduledTimer(withTimeInterval: coinFlowDuration, repeats: false) { _ in
-            coinFlowTimer.invalidate()
-        }
-    }
+//    func dropCoins(parentNode:SCNNode) {
+//        let numberOfCoins:Double = 100
+//        let coinsPerSecond:Double = 8
+//        let coinInterval:Double = 1.0 / coinsPerSecond
+//        let coinFlowDuration:Double = numberOfCoins/coinsPerSecond
+//
+//        let coinFlowTimer = Timer.scheduledTimer(withTimeInterval: coinInterval, repeats: true) { _ in
+//            parentNode.addChildNode(self.newCoin())
+//        }
+//
+//        _ = Timer.scheduledTimer(withTimeInterval: coinFlowDuration, repeats: false) { _ in
+//            coinFlowTimer.invalidate()
+//        }
+//    }
     
-    func newCoin() -> SCNNode {
+    func newCoin(position:SCNVector3) -> SCNNode {
         let coin = SCNCylinder(radius: 0.2, height: 0.04)
         coin.radialSegmentCount = 24
         
@@ -164,7 +159,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         coin.firstMaterial?.metalness.contents = NSNumber(value: 1.0)
         
         let coinNode = SCNNode(geometry: coin)
-        coinNode.position = SCNVector3(x: 0.0, y: 1.5, z: 0.0)
+        coinNode.position = position
+        coinNode.position.y += 1.5
         coinNode.eulerAngles = SCNVector3(x: .pi * randomAroundZero(), y: .pi * randomAroundZero(), z: .pi * randomAroundZero())
         
         let coinPhysicsShape = SCNPhysicsShape(geometry: coin, options: nil)
